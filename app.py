8222034be611f830ca80b4d37b547ed9c1377cbb
Youssef Doss <youssefdoss97@gmail.com>
2022-12-12 15:02:30 -0500

    finish first route

--- app.py
+++ app.py
@@ -1,1 +1,21 @@
 """Flask app for Cupcakes"""
+
+from flask import Flask, request, jsonify
+from models import db, connect_db, Cupcake
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+
+connect_db(app)
+
+
+@app.get('/api/cupcakes')
+def list_all_cupcakes():
+    '''Return JSON of all cupcakes {'cupcakes': [{id, ...}, ...]}'''
+
+    cupcakes = Cupcake.query.all()
+    serialized = [c.serialize() for c in cupcakes]
+
+    return jsonify(cupcakes=serialized)
