9367db7d679fb4a3627b1d84a64a741c39a64dd7
Youssef Doss <youssefdoss97@gmail.com>
2022-12-12 19:20:09 -0500

    final commit

--- static/main.js
+++ static/main.js
@@ -1,14 +1,18 @@
 "use strict";
 
 let $cupcakeList = $("ul");
-
+// Add a global constant for the base url
+/** getCupcakeList: Returns all cupcakes from the API */
 async function getCupcakeList() {
+  // Just return response.data.cupcakes
   return await axios.get("http://localhost:5001/api/cupcakes");
 }
 
+/** addCupcakesToPage: Append all cupcakes to the UI */
 async function addCupcakesToPage() {
+  $cupcakeList.empty();
   let response = await getCupcakeList();
-  console.log(response);
+
   for (let i = 0; i < response.data.cupcakes.length; i++) {
     let cupcake = response.data.cupcakes[i];
     $cupcakeList.append(`
@@ -23,5 +27,22 @@ async function addCupcakesToPage() {
   }
 }
 
-// let response = await getCupcakeList();
+/** getFormValues: Pull all values out of the form */
+function getFormValues() {
+  const flavor = $('#flavor').val();
+  const size = $('#size').val();
+  const rating = $('#rating').val();
+  const image = $('#image').val();
+
+  return {flavor: flavor, size: size, rating: rating, image: image};
+}
+
+async function createCupcake() {
+  const formValues = getFormValues();
+  await axios.post('http://localhost:5001/api/cupcakes', formValues);
+  await addCupcakesToPage();
+}
+
+$('button').on('click', createCupcake);
+
 addCupcakesToPage();
